18cec712e85e855d52cd9e944036022595e9d994
Maxime Moreillon <m.moreillon@gmail.com>
2022-01-24 20:13:01 +0900

    env

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
-// Load config from .env.local
+// Load config from environment
 const {
     VUE_APP_FIREBASE_API_KEY,
     VUE_APP_FIREBASE_AUTH_DOMAIN,
@@ -10,6 +10,7 @@ const {
     VUE_APP_FIREBASE_STORAGE_BUCKET,
     VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
     VUE_APP_FIREBASE_APP_ID,
+    VUE_APP_FIREBASE_MEASUREMENT_ID,
 } = process.env
 
 
@@ -22,6 +23,7 @@ const firebaseConfig = {
     storageBucket: VUE_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: VUE_APP_FIREBASE_APP_ID,
+    measurementId: VUE_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
